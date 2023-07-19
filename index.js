4271b83f163bfd72518db3bfb4346eef4aa8305f
Abhishek Kumar Pandey <abhishekpandey7676@gmail.com>
2023-07-19 11:02:08 +0530

    chnages

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('Hello World');
+
+num = 35;
+console.log(num);
+
+msg = "Nice";
+console.log(msg);
+
+console.log(typeof(num));
+console.log(typeof(msg));
+
+age = 30;
+
+if(age >= 18)
+   console.log('Eligible for DL')
+else
+   console.log('Not Eligible for DL')
