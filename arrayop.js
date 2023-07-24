c8deff106d6339e0d2555fb9682600e666ac77b9
Abhishek Kumar Pandey <abhishekpandey7676@gmail.com>
2023-07-25 02:22:47 +0530

    changes

--- arrayop.js
+++ arrayop.js
@@ -1,27 +1,27 @@
-// const nums=[23453 , 46 , 23 , 39 , 3 , 8];
+const nums=[23453 , 46 , 23 , 39 , 3 , 8];
 
-// const newnums = [];
-// for(let i of nums){
-//     console.log(i ** 2);
-//     newnums.push(i ** 2);
-// }
+const newnums = [];
+for(let i of nums){
+    console.log(i ** 2);
+    newnums.push(i ** 2);
+}
 
-// console.log(newnums);
-
-
-// const result = nums.map( (n) => { return n**2 } );
-// console.log(result);
-
-// const prices = [3400 , 2347 , 27 , 494 , 988];
-
-// const result2 = prices.map( (p) => {return p + p*0.1} );
-// console.log(result2);
+console.log(newnums);
 
 
-// // use map to get discounted prices = 7.5% discount
+const result = nums.map( (n) => { return n**2 } );
+console.log(result);
 
-// const result3 = prices.map( (p) => {return p*0.925} );
-// console.log(result3);
+const prices = [3400 , 2347 , 27 , 494 , 988];
+
+const result2 = prices.map( (p) => {return p + p*0.1} );
+console.log(result2);
+
+
+// use map to get discounted prices = 7.5% discount
+
+const result3 = prices.map( (p) => {return p*0.925} );
+console.log(result3);
 
 // use map to get the prices in integer format
 const prices2 = [ '$67.89' , '$348.99' , '$29384.22' , '$4.102' , '$1.242'];
